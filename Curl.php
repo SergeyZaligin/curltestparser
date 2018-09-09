6d197baa6c06a125ec93914919a64dfbb0a54582
Sergey Zaligin <webdevjsphp@gmail.com>
2018-09-09 17:29:21 +0300

    init

--- Curl.php
+++ Curl.php
@@ -59,4 +59,43 @@ class Curl
      }
       return $this->host . $url;
    }
+   /**
+    * Метод преобразует $data, отделяя заголовки от тела сообщения
+    */
+   private function processResult($data)
+   {
+      $p_n = "\n";
+      $p_rn = "\r\n";
+
+      $h_end_n = strpos($data, $p_n . $p_n); // int or false
+      $h_end_rn = strpos($data, $p_rn . $p_rn); // int or false
+      $start = $h_end_n; // h_end_n int
+		$p = $p_n;		 // \n
+
+		if($h_end_n === false || $h_end_rn < $h_end_n){
+			$start = $h_end_rn;
+			$p = $p_rn;
+		}
+
+		$headers_part = substr($data, 0, $start);
+		$body_part = substr($data, $start + strlen($p) * 2);
+
+		$lines = explode($p, $headers_part);
+		$headers = array();
+
+		$headers['start'] = $lines[0];
+
+		for($i = 1; $i < count($lines); $i++){
+			$del_pos = strpos($lines[$i], ':');
+			$name = substr($lines[$i], 0, $del_pos);
+			$value = substr($lines[$i], $del_pos + 2);
+			$headers[$name] = $value;
+		}
+
+		return array(
+			'headers' => $headers,
+			'html' => $body_part
+		);
+
+   }
 }
--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 include_once 'Curl.php';
 $c = Curl::app('http://ipap.ru')
-    ->set(CURLOPT_HEADER, 0);
-$html = $c->request('news');
+    ->set(CURLOPT_HEADER, 1)
+    ->set(CURLOPT_FOLLOWLOCATION, false);
 
+//$html = $c->request('svedeniya-ob-ipap/otzyvy'); // 301 redirect /otzyvy
+$html = $c->request('news');
 echo $html;
