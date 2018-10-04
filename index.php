730613e09b91ab1683457e06a877a3001b3d02d7
sergey <sergey>
2018-10-04 14:20:16 +0300

    fix

--- index.php
+++ index.php
@@ -1,18 +1,15 @@
 <?php
 include_once 'Curl.php';
-//$c = Curl::app('http://ipap.ru')
-//    ->set(CURLOPT_HEADER, 1)
-//    ->set(CURLOPT_FOLLOWLOCATION, false);
+
 $c = Curl::app('http://agencytop.loc')
         ->headers(1)
         ->http(false)
         ->post(true)
         ->cookie('/1.txt');
-//$html = $c->request('svedeniya-ob-ipap/otzyvy'); // 301 redirect /otzyvy
+
 $data = $c->request('user/login');
 $data = $c->request('/');
-//secho $data['html'];
-//echo $data['headers'];
+
 //echo "<pre>";
 print_r($data);
 //echo "</pre>";
