91d9115e9a8ff14d2402d5dc3f77f609fd9de107
Sergey Zaligin <webdevjsphp@gmail.com>
2018-10-07 12:50:54 +0300

    fix

--- countries_list.php
+++ countries_list.php
@@ -2,25 +2,39 @@
 
 include_once('lib/Curl.php');
 include_once('lib/simple_html_dom.php');
-	
+
+
+// Load page with wiki.conf
 $c = Curl::app('https://en.wikipedia.org')->config_load('wiki.cnf');
 					
-
+// Get array data on data['headers'] and data['html']
 $data = $c->request('wiki/List_of_sovereign_states');
+
+// Build DOM
 $dom = str_get_html($data['html']);
-    
+
+// Find parser items $flags - array objects    
 $flags = $dom->find('.flagicon');
+
+// Count items
 $done = 0;
+
+// Array countries
 $countries = [];
-    
+
+// Iteration $flags
 foreach ($flags as $span) {
+    
     $b = $span->parent();
 
-    if($b->tag != 'b')
-    continue;
-
+    if($b->tag != 'b'){
+        continue;
+    }
+    
     $a = $b->find('a', 0);
-    $countries[$a->href] = $a->plaintext;            
+    
+    $countries[$a->href] = $a->plaintext;  
+    
     $done++;       
 }
     
